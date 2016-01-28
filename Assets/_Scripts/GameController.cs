ec3cabbab3a2ce0327eb037c0ef14857bd5a010c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-01-27 19:52:44 -0500

    added initial version of GameController

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int cloudNumber = 3;
+	public CloudController cloud;
+
+
+	// Use this for initialization
+	void Start () {
+		this._initialize ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//PRIVATE METHODS ++++++++++++++++++
+
+	//Initial Method
+	private void _initialize() {
+		for (int cloudCount = 0; cloudCount < this.cloudNumber; cloudCount++) {
+			Instantiate (cloud.gameObject);
+		}
+	}
+}
